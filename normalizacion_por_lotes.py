6d33ed183336a153bfcbc84b6ff5d678d841999e
lifiano <martmauri29@hotmail.com>
2024-11-13 12:38:19 -0300

    actualizo

--- normalizacion_por_lotes.py
+++ normalizacion_por_lotes.py
@@ -7,80 +7,63 @@ from geolocalizador import GeolocalizadorDatosGobar  # Importar desde el otro ar
 import sys
 import os
 
+
 sys.path.append(os.path.abspath('C:/Users/Usuario/appdata/local/programs/python/python310/lib/site-packages'))
 
-
-input_file = 'C:/Users/Usuario/Desktop/ovs-caminero/direcciones_BsAs.csv'
-output_file = 'C:/Users/Usuario/Desktop/ovs-caminero/direcciones_normalizadas_por_lotes.csv'
+input_file = '/home/lifiano/Escritorio/Martin/OVS-caminero/direcciones_BsAs.csv'
+output_file = '/home/lifiano/Escritorio/Martin/OVS-caminero/direcciones_normalizadas_por_lotes.csv'
 
 df = pd.read_csv(input_file, sep=',', keep_default_na=False, on_bad_lines='skip')
 df.columns = df.columns.str.strip()
 
-
 # Filtrar direcciones no nulas y no vacías
 df = df[df['address'].notna() & (df['address'].str.strip() != '')]
 direcciones = df[['address', 'district']].dropna().values.tolist()
-
-direcciones = direcciones[:100000]
-
+direcciones = direcciones[:150000]
 
 # Función para procesar la dirección y eliminar cualquier altura igual a '0'
 def procesar_direccion(direccion):
-    # Eliminar llaves y limpiar la dirección
     direccion = direccion.replace('{', '').replace('}', '')
-    
-    # Eliminar ceros sueltos o secuencias de ceros
     direccion_limpia = re.sub(r'\b00+\b|\b0\b', '', direccion).strip()
-    
-    # Verificar si la dirección está vacía después del procesamiento
-    if not direccion_limpia:
-        return None  # Si está vacía, retornar None
-    
-    return direccion_limpia  # Retornar la dirección limpia si no está vacía
+    return direccion_limpia if direccion_limpia else None
 
-
-# Función para normalizar direcciones por lotes con el parámetro "max"
+# Función para normalizar direcciones por lotes
 def normalizar_direcciones(direcciones):
     base_url = "https://apis.datos.gob.ar/georef/api/direcciones"
     resultados = []
 
-    for i in range(0, len(direcciones),1000):
-        lote = direcciones[i:i+1000]
+    for i in range(0, len(direcciones), 1000):
+        lote = direcciones[i:i + 1000]
         payload = {
             "direcciones": []
         }
 
         for dir in lote:
-            direccion_procesada = procesar_direccion(dir[0])  # Procesar cada dirección
-            if direccion_procesada:  # Verificar que la dirección no esté vacía
+            direccion_procesada = procesar_direccion(dir[0])
+            if direccion_procesada:
                 direccion_data = {
                     "direccion": direccion_procesada,
-                    "max": 3  # Limitar los resultados a un máximo de 5 por dirección
+                    "max": 3
                 }
-                # Si hay distrito, añadirlo también al payload
                 if dir[1]:
                     direccion_data["localidad_censal"] = dir[1]
-                
                 payload["direcciones"].append(direccion_data)
-    
 
-        if not payload["direcciones"]:  # Si no hay direcciones válidas en el lote, continuar
+        if not payload["direcciones"]:
             print(f"Lote {i // 900 + 1} está vacío después del procesamiento.")
             continue
 
         response = requests.post(base_url, headers={'Content-Type': 'application/json'}, data=json.dumps(payload))
-        
-        # Verificar la respuesta de la API
+
         if response.status_code == 200:
             data = response.json()
             resultados.extend(data['resultados'])
         else:
-            # Imprimir el lote que causó el error
             print(f"Error en el lote {i // 900 + 1}: {response.status_code}, Mensaje: {response.text}")
-            print(f"Lote que causó el error: {payload}")  # Imprimir el contenido del lote en caso de error
-        
-        time.sleep(1)
-    
+            print(f"Lote que causó el error: {payload}")
+
+        time.sleep(0)
+
     return resultados
 
 # Normalización y geolocalización
@@ -112,9 +95,8 @@ for i, resultado in enumerate(resultados_normalizados):
 
 # Guardar los resultados normalizados en un archivo CSV
 df_normalizado = pd.DataFrame(normalizadas)
-# Eliminar duplicados
 df_normalizado = df_normalizado.drop_duplicates(subset=['direccion', 'localidad', 'latitud', 'longitud'], keep='first')
-# Guardarlo
 df_normalizado.to_csv(output_file, index=False)
 
 print("Proceso completado. Resultados guardados en:", output_file)
+
