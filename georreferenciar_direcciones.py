97bab6c60ba1ad80582add76c161bdbea7a70867
mmaurino <martmauri29@hotmail.com>
2024-11-22 14:51:50 -0300

    Agrego cuadro para seleccionar cantidad de direcciones

--- georreferenciar_direcciones.py
+++ georreferenciar_direcciones.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
 from geolocalizador import GeolocalizadorDatosGobar
 
 # Función para seleccionar un archivo de entrada
@@ -16,6 +16,13 @@ def seleccionar_archivo_salida():
     root.withdraw()  # Ocultar la ventana principal
     archivo = filedialog.asksaveasfilename(title="Guardar archivo CSV de direcciones normalizadas", defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
     return archivo
+
+# Función para pedir al usuario el número de direcciones a procesar
+def obtener_numero_direcciones_maximas():
+    root = tk.Tk()
+    root.withdraw()  # Ocultar la ventana principal
+    max_direcciones = simpledialog.askinteger("Cantidad de direcciones", "¿Cuántas direcciones deseas procesar?", minvalue=1)
+    return max_direcciones
 
 # Seleccionar el archivo de entrada
 input_file = seleccionar_archivo_entrada()
@@ -36,7 +43,12 @@ df.columns = df.columns.str.strip()
 # Filtrar direcciones no nulas y no vacías
 df = df[df['address'].notna() & (df['address'].str.strip() != '')]
 direcciones = df[['address', 'district']].dropna().values.tolist()
-direcciones = direcciones[:1000]
+
+# Obtener el número de direcciones a procesar
+max_direcciones = obtener_numero_direcciones_maximas()
+
+# Limitar las direcciones a procesar
+direcciones = direcciones[:max_direcciones] if max_direcciones else direcciones
 
 # Crear instancia del geolocalizador
 geolocalizador = GeolocalizadorDatosGobar(0)
